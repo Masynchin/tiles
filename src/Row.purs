fc3a697678cbef287e4812c2593f6e2b3f22da9f
Max Smirnov <masynchin@gmail.com>
2022-05-23 10:40:18 +0300

    Wrap cons and snoc for Rows with meaningful functions

--- src/Row.purs
+++ src/Row.purs
@@ -16,10 +16,16 @@ import Tile (Tile, connectsHorizontal, empty)
 type FieldRow = NonEmptyList Tile
 
 rowCompleted :: FieldRow -> Boolean
-rowCompleted row = and $ zipWith connectsHorizontal (cons empty row) (snoc row empty)
+rowCompleted row = and $ zipWith connectsHorizontal (prependTile empty row) (appendTile row empty)
+
+prependTile :: Tile -> FieldRow -> FieldRow
+prependTile = cons
+
+appendTile :: FieldRow -> Tile -> FieldRow
+appendTile = snoc
 
 row2 :: Tile -> Tile -> FieldRow
-row2 t1 t2 = cons t1 $ row1 t2
+row2 t1 t2 = prependTile t1 $ row1 t2
 
 row1 :: Tile -> FieldRow
 row1 = singleton
